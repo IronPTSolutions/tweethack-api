1acae3974f5f96c28a5c147358a4e39a2b797ee0
Pablo Laso Cortabitarte <plasocortabitarte@gmail.com>
2024-02-12 20:05:54 +0100

    feat: login feature

--- config/routes.config.js
+++ config/routes.config.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const usersController = require('../controllers/users.controller');
+const authController = require('../controllers/auth.controller');
 
 // Auth
+router.post('/login', authController.login);
 
 // Users
 router.post('/users', usersController.create);
