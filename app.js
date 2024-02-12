029876c0f811cb03bc0eb5bd17918883893580a0
Pablo Laso Cortabitarte <plasocortabitarte@gmail.com>
2024-02-12 19:00:38 +0100

    feat: added error handling middlewares

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const logger = require('morgan');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
+const createError = require('http-errors');
+const { StatusCodes } = require('http-status-codes');
 
 // Base de datos
 
@@ -24,6 +26,44 @@ app.use(logger('dev'));
 
 const routes = require('./config/routes.config');
 app.use('/api', routes);
+
+/* Handle errors */
+
+// Middleware para cuando no encuentra ruta
+app.use((req, res, next) => {
+  next(createError(StatusCodes.NOT_FOUND, "Route not found"));
+});
+
+// Middleware genérico de errores
+app.use((error, req, res, next) => {
+  console.error(error);
+
+  if (error instanceof mongoose.Error.ValidationError) {
+    error = createError(400, error);
+  } else if (error instanceof mongoose.Error.CastError) {
+    error = createError(400, "Resource not found");
+  } else if (error.message.includes("E11000")) {
+    error = createError(400, "Resource already exists");
+  } else if (error instanceof jwt.JsonWebTokenError) {
+    error = createError(401, error);
+  } else if (!error.status) {
+    error = createError(500);
+  }
+
+  const data = {};
+
+  data.message = error.message;
+  data.errors = error.errors
+    ? Object.keys(error.errors).reduce((errors, key) => {
+        return {
+          ...errors,
+          [key]: error.errors[key].message || error.errors[key],
+        };
+      }, {})
+    : undefined;
+
+  res.status(error.status).json(data);
+});
 
 // Arranque del servidor
 
